5960a2675d4dfa41fca99452c868651e02b42a69
samiksha275 <agarwalsamiksha88@gmail.com>
2025-11-07 09:49:08 +0530

    Database connected

--- backend/app.js
+++ backend/app.js
@@ -1,4 +1,7 @@
 const express = require('express')
+const { connectdb } = require('./db')
 const app = express()
 
+require('dotenv').config()
+connectdb()
 app.listen(3000, () => console.log("Server started"))
